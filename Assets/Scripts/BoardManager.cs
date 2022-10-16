8137b62d9a5748835ab07e786508a10917bbc787
Izabela-Zelek <C00247865@itcarlow.ie>
2022-10-16 17:57:50 +0100

    Started multiple room generation

--- Assets/Scripts/BoardManager.cs
+++ Assets/Scripts/BoardManager.cs
@@ -5,21 +5,9 @@ using Random = UnityEngine.Random;
 
 public class BoardManager : MonoBehaviour
 {
-	[Serializable]
-	public class Count
-	{
-		public int minimum;		
-		public int maximum;
-
-		public Count (int min, int max)
-		{
-			minimum = min;
-			maximum = max;
-		}
-	}
-		
-	public int columns = 8; 
-	public int rows = 8;
+	
+	private int columns; 
+	private int rows;
 	public GameObject exit;											
 	public GameObject[] floorTiles;							
 	public GameObject[] outerWallTiles;							
@@ -33,6 +21,8 @@ public class BoardManager : MonoBehaviour
 	//Clears our list gridPositions and prepares it to generate a new board.
 	void InitialiseList ()
 	{
+		columns = Random.Range(4, 7);
+		rows = Random.Range(4, 7);
 		//Clear our list gridPositions.
 		gridPositions.Clear ();
 		cornerPositions.Clear();
@@ -54,7 +44,7 @@ public class BoardManager : MonoBehaviour
 		
 		
 	//Sets up the outer walls and floor (background) of the game board.
-	void BoardSetup ()
+	void BoardSetup (int boardNr,int boardOffsetX, int boardOffsetY)
 	{
         Debug.Log(cornerPositions[0]);
         Debug.Log(cornerPositions[1]);
@@ -64,15 +54,15 @@ public class BoardManager : MonoBehaviour
 		//Instantiate Board and set boardHolder to its transform.
 		boardHolder = new GameObject ("Board").transform;
 			
-		for(int x = -1; x < columns + 1; x++)
+		for(int x = -1 + boardOffsetX; x < columns + 1 + boardOffsetX; x++)
 		{
-			for(int y = -1; y < rows + 1; y++)
+			for(int y = -1 + boardOffsetY; y < rows + 1 + boardOffsetY; y++)
 			{
 				//Choose a random tile from our array of floor tile prefabs and prepare to instantiate it.
 				GameObject toInstantiate = floorTiles[Random.Range (0,floorTiles.Length)];
 
 				//Check if we current position is at board edge, if so choose a random outer wall prefab from our array of outer wall tiles.
-				if (x == -1 || x == columns || y == -1 || y == rows)
+				if (x == -1 +  boardOffsetX || x == columns + boardOffsetX || y == -1 + boardOffsetY || y == rows + boardOffsetY)
 				{
 					toInstantiate = outerWallTiles[Random.Range(0, outerWallTiles.Length)];
 				}
@@ -80,7 +70,7 @@ public class BoardManager : MonoBehaviour
 
 				for (int i = 0; i < cornerPositions.Count; i++)
 				{
-					if (x == -1 || x == columns || y == -1 || y == rows)
+					if (x == -1 + boardOffsetX|| x == columns + boardOffsetX|| y == -1 + boardOffsetY|| y == rows + boardOffsetY)
 					{
 						if (x != cornerPositions[i].x && y != cornerPositions[i].y)
 						{
@@ -97,28 +87,44 @@ public class BoardManager : MonoBehaviour
 	}
 	public void SetupScene ()
 	{
-		InitialiseList();
+		int randRoomNr = Random.Range(3, 5);
+		int offsetX = 0;
+		int offsetY = 0;
 
-		BoardSetup();
-
-		Vector3 exitPos = outerWallPositions[Random.Range(0, outerWallPositions.Count)];
-		for (int i = 0; i < cornerPositions.Count; i++)
+		for (int j = 0; j < randRoomNr; j++)
 		{
-			if (exitPos.x == cornerPositions[i].x && exitPos.y == cornerPositions[i].y)
+			int prevCol = columns;
+			int prevRow = rows;
+
+			InitialiseList();
+
+			if (j != 0)
 			{
-				exitPos = outerWallPositions[Random.Range(0, outerWallPositions.Count)];
+				offsetX = offsetX + columns + prevCol;
+				offsetY = offsetY + rows + prevRow;
+			}
+
+			BoardSetup(j,offsetX,offsetY);
+
+			Vector3 exitPos = outerWallPositions[Random.Range(0, outerWallPositions.Count)];
+			for (int i = 0; i < cornerPositions.Count; i++)
+			{
+				if (exitPos.x == cornerPositions[i].x && exitPos.y == cornerPositions[i].y)
+				{
+					exitPos = outerWallPositions[Random.Range(0, outerWallPositions.Count)];
+				}
+			}
+
+			Instantiate(exit, exitPos, Quaternion.identity);
+
+			//Finds the wall sprite hidden behind door and sets it to inactive, allows player to pass through door
+			for (int i = 0; i < boardHolder.childCount; i++)
+			{
+				if (boardHolder.GetChild(i).transform.position == exitPos)
+				{
+					boardHolder.GetChild(i).gameObject.SetActive(false);
+				}
 			}
 		}
-
-		Instantiate (exit, exitPos, Quaternion.identity);
-
-		//Finds the wall sprite hidden behind door and sets it to inactive, allows player to pass through door
-		for (int i = 0; i < boardHolder.childCount;i++)
-        {
-			if(boardHolder.GetChild(i).transform.position == exitPos)
-            {
-				boardHolder.GetChild(i).gameObject.SetActive(false);
-			}
-        }
 	}
 }
