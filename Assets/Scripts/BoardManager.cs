e4402615e4c8b983eef6dc4ca03efe542b9a3d5a
Izabela-Zelek <C00247865@itcarlow.ie>
2022-10-18 23:00:00 +0100

    Double room generation now possible

--- Assets/Scripts/BoardManager.cs
+++ Assets/Scripts/BoardManager.cs
@@ -16,13 +16,20 @@ public class BoardManager : MonoBehaviour
 	private List <Vector3> gridPositions = new List <Vector3> ();
 	private List<Vector3> outerWallPositions = new List<Vector3>();
 	private List<Vector3> cornerPositions = new List<Vector3>();
-		
-		
-	//Clears our list gridPositions and prepares it to generate a new board.
-	void InitialiseList ()
+	private List<int> possibleSize = new List<int>();
+
+    private void Awake()
+    {
+		possibleSize.Add(4);
+		possibleSize.Add(6);
+		possibleSize.Add(8);
+
+	}
+    //Clears our list gridPositions and prepares it to generate a new board.
+    void InitialiseList (int offsetX,int offsetY)
 	{
-		columns = Random.Range(4, 7);
-		rows = Random.Range(4, 7);
+		columns = possibleSize[Random.Range(0, possibleSize.Count)];
+		rows = possibleSize[Random.Range(0, possibleSize.Count)];
 		//Clear our list gridPositions.
 		gridPositions.Clear ();
 		cornerPositions.Clear();
@@ -36,33 +43,37 @@ public class BoardManager : MonoBehaviour
 				gridPositions.Add (new Vector3(x, y, 0f));
 			}
 		}
-		cornerPositions.Add(new Vector3(-1, -1, 0f));
-		cornerPositions.Add(new Vector3(-1, rows, 0f));
-		cornerPositions.Add(new Vector3(columns, -1, 0f));
-		cornerPositions.Add(new Vector3(columns, rows, 0f));
+		cornerPositions.Add(new Vector3(offsetX - 1, offsetY - 1, 0f));
+		cornerPositions.Add(new Vector3(offsetX - 1, offsetY + rows, 0f));
+		cornerPositions.Add(new Vector3(offsetX + columns, offsetY - 1, 0f));
+		cornerPositions.Add(new Vector3(offsetX + columns, offsetY + rows, 0f));
 	}
 		
 		
 	//Sets up the outer walls and floor (background) of the game board.
 	void BoardSetup (int boardNr,int boardOffsetX, int boardOffsetY)
 	{
-        Debug.Log(cornerPositions[0]);
-        Debug.Log(cornerPositions[1]);
-        Debug.Log(cornerPositions[2]);
-        Debug.Log(cornerPositions[3]);
+		int boardOriginX = 0;
+		int boardOriginY = 0;
 
 		//Instantiate Board and set boardHolder to its transform.
 		boardHolder = new GameObject ("Board").transform;
+
+		if(boardNr != 0)
+        {
+			boardOriginX = boardOffsetX;
+			boardOriginY = boardOffsetY;
+		}
 			
-		for(int x = -1 + boardOffsetX; x < columns + 1 + boardOffsetX; x++)
+		for(int x = -1 + boardOriginX; x < columns + 1 + boardOriginX; x++)
 		{
-			for(int y = -1 + boardOffsetY; y < rows + 1 + boardOffsetY; y++)
+			for(int y = -1 + boardOriginY; y < rows + 1 + boardOriginY; y++)
 			{
 				//Choose a random tile from our array of floor tile prefabs and prepare to instantiate it.
 				GameObject toInstantiate = floorTiles[Random.Range (0,floorTiles.Length)];
 
 				//Check if we current position is at board edge, if so choose a random outer wall prefab from our array of outer wall tiles.
-				if (x == -1 +  boardOffsetX || x == columns + boardOffsetX || y == -1 + boardOffsetY || y == rows + boardOffsetY)
+				if (x == -1 + boardOriginX || x == columns + boardOriginX || y == -1 + boardOriginY || y == rows + boardOriginY)
 				{
 					toInstantiate = outerWallTiles[Random.Range(0, outerWallTiles.Length)];
 				}
@@ -70,7 +81,7 @@ public class BoardManager : MonoBehaviour
 
 				for (int i = 0; i < cornerPositions.Count; i++)
 				{
-					if (x == -1 + boardOffsetX|| x == columns + boardOffsetX|| y == -1 + boardOffsetY|| y == rows + boardOffsetY)
+					if (x == -1 + boardOriginX || x == columns + boardOriginX || y == -1 + boardOriginY || y == rows + boardOriginY)
 					{
 						if (x != cornerPositions[i].x && y != cornerPositions[i].y)
 						{
@@ -88,34 +99,66 @@ public class BoardManager : MonoBehaviour
 	public void SetupScene ()
 	{
 		int randRoomNr = Random.Range(3, 5);
+		randRoomNr = 2;
 		int offsetX = 0;
 		int offsetY = 0;
+		string exitSide = "None";
+
+		Vector3 exitPos = new Vector3( 0, 0, 0 );
 
 		for (int j = 0; j < randRoomNr; j++)
 		{
-			int prevCol = columns;
-			int prevRow = rows;
 
-			InitialiseList();
-
-			if (j != 0)
-			{
-				offsetX = offsetX + columns + prevCol;
-				offsetY = offsetY + rows + prevRow;
-			}
+			InitialiseList(offsetX,offsetY);
 
 			BoardSetup(j,offsetX,offsetY);
 
-			Vector3 exitPos = outerWallPositions[Random.Range(0, outerWallPositions.Count)];
+			if (j==0)
+            {
+				exitPos = outerWallPositions[Random.Range(0, outerWallPositions.Count)];
+				clearExit(exitPos);
+			}
+
+			if (exitPos.y == offsetY + rows)
+            {
+				exitSide = "North";
+				offsetX = (int)exitPos.x - Mathf.FloorToInt(columns / 2);
+				offsetY = (int)exitPos.y + 1;
+			}
+			else if (exitPos.y == offsetY - 1)
+			{
+				exitSide = "South";
+				offsetX = (int)exitPos.x - Mathf.FloorToInt(columns / 2);
+				offsetY = (int)exitPos.y - rows;
+			}
+			else if (exitPos.x== offsetX + columns)
+			{
+				exitSide = "East";
+				offsetX = (int)exitPos.x + 1;
+				offsetY = (int)exitPos.y - Mathf.FloorToInt(rows / 2);
+			}
+			else if (exitPos.x == offsetX - 1)
+			{
+				exitSide = "West";
+				offsetX = (int)exitPos.x - columns ;
+				offsetY = (int)exitPos.y - Mathf.FloorToInt(rows / 2);
+			}
+			Debug.Log(exitSide);
+
+			clearExit(exitPos);
+			if (j != 0)
+			{
+				exitPos = outerWallPositions[Random.Range(0, outerWallPositions.Count)];
+			}
 			for (int i = 0; i < cornerPositions.Count; i++)
 			{
-				if (exitPos.x == cornerPositions[i].x && exitPos.y == cornerPositions[i].y)
+				while (exitPos.x == cornerPositions[i].x && exitPos.y == cornerPositions[i].y)
 				{
 					exitPos = outerWallPositions[Random.Range(0, outerWallPositions.Count)];
 				}
 			}
 
-			Instantiate(exit, exitPos, Quaternion.identity);
+			Instantiate(floorTiles[Random.Range(0, floorTiles.Length)], exitPos, Quaternion.identity);
 
 			//Finds the wall sprite hidden behind door and sets it to inactive, allows player to pass through door
 			for (int i = 0; i < boardHolder.childCount; i++)
@@ -124,6 +167,17 @@ public class BoardManager : MonoBehaviour
 				{
 					boardHolder.GetChild(i).gameObject.SetActive(false);
 				}
+			}
+		}
+	}
+
+	private void clearExit(Vector3 exit)
+    {
+		for (int i = 0; i < boardHolder.childCount; i++)
+		{
+			if (boardHolder.GetChild(i).transform.position == exit)
+			{
+				Destroy(boardHolder.GetChild(i).gameObject);
 			}
 		}
 	}
