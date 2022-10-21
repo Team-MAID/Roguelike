0f01fcd7d0bfd63092f37c232c2bfa331eed8114
Patrick Donnelly <45255674+PatrickDonnelly@users.noreply.github.com>
2022-10-21 11:58:18 +0100

    Addeed Spider nest as a spawner. Spawns a spider every five seconds. Each spider follows the player if the player is within attack range. Spider and spider nest saved as prefabs. Added a spider nest spawner script Still trying to rebase

--- Assets/Scripts/SpiderSpawner.cs
+++ Assets/Scripts/SpiderSpawner.cs
@@ -4,15 +4,28 @@ using UnityEngine;
 
 public class SpiderSpawner : MonoBehaviour
 {
+
+    [SerializeField]
+    private GameObject spiderPrefab;
+
+    [SerializeField]
+    private GameObject spiderNest;
+
+    [SerializeField]
+    private float spawnInterval = 5.0f;
+
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        StartCoroutine(spawnEnemy(spawnInterval, spiderPrefab));
     }
 
-    // Update is called once per frame
-    void Update()
+    private IEnumerator spawnEnemy(float interval, GameObject enemy)
     {
-        
+        yield return new WaitForSeconds(interval);
+        GameObject newEnemy = Instantiate(enemy, new Vector3(spiderNest.transform.position.x, spiderNest.transform.position.y, 0), Quaternion.identity);
+        StartCoroutine(spawnEnemy(interval,enemy));
     }
 }
