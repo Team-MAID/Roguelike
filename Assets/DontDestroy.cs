1b69d54fdfd17da1b852b08935f02ce1bc87475d
Patrick Donnelly <45255674+PatrickDonnelly@users.noreply.github.com>
2022-12-16 15:55:39 +0000

    fixed a bug that was introduced with Dontdestroy that was causing duplicate huds and companions on restarting levels after a game over. And duplicate companion, player and hud after clearing the final level and restarting

--- Assets/DontDestroy.cs
+++ Assets/DontDestroy.cs
@@ -1,12 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class DontDestroy : MonoBehaviour
 {
+    private GameObject _player;
     // Start is called before the first frame update
     private void Awake()
     {
+        _player = GameObject.FindWithTag("Player");
         DontDestroyOnLoad(this);
+    }
+
+
+    private void Update()
+    {
+        // Destroy when cleared to avoid duplicate players and companions on restart
+        if (SceneManager.GetActiveScene().name == "Cleared")
+        {
+            Destroy(this.gameObject);
+        }
+        if (_player == null)
+        {
+            Debug.Log("Arrived");
+            Destroy(this.gameObject);
+        }
     }
 }
